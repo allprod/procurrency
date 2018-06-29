d3384587529a6e2e46a8ac4a36dedb6625d2fcb7
Entropy <paulsmsiame@gmail.com>
2018-06-29 21:17:55 +0200

    moving laptops

--- static/js/sw.js
+++ static/js/sw.js
@@ -4,6 +4,10 @@ const all_caches = [
 ];
 const cache_urls = [
     '/',
+    '/index.html',
+    'static/js/idb.js',
+    'static/js/main.js',
+    'static/css/main.css',
     'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
     'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
 ];
@@ -22,6 +26,7 @@ self.addEventListener('activate', event => {
             )
         )
     );
+    self.clients.claim();
 });
 
 
