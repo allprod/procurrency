009c32171d467867fc3264e477a255fc7ae82cd7
Entropy <paulsmsiame@gmail.com>
2018-06-29 23:42:23 +0200

    fixed relative link bug

--- static/js/sw.js
+++ static/js/sw.js
@@ -3,22 +3,23 @@ const all_caches = [
     static_cache_name
 ];
 const cache_urls = [
-    './',
-    './index.html',
-    './static/js/idb.js',
-    './static/js/main.js',
-    './static/css/main.css',
+    '../../',
+    '../../index.html',
+    'idb.js',
+    'main.js',
+    '../css/main.css',
     'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
     'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(caches.open(static_cache_name).then(cache => cache.addAll(cache_urls))
-    );
+    console.log('[service worker]: installing sw');
+    event.waitUntil(caches.open(static_cache_name).then(cache => cache.addAll(cache_urls)));
 });
 
 
 self.addEventListener('activate', event => {
+    console.log('[service worker]: activating sw');
     event.waitUntil(
         caches.keys().then(cache_names => Promise.all(
                 cache_names.filter(cache_name => cache_name.startsWith('procurrency-') && !all_caches.includes(cache_name)
@@ -31,11 +32,13 @@ self.addEventListener('activate', event => {
 
 
 self.addEventListener('fetch', event => {
+    console.log('[service worker]: fetching from sw');
     event.respondWith(
         caches.match(event.request).then(response => response || fetch(event.request))
     );
 });
 
 self.addEventListener('message', event => {
+    console.log('[service worker]: handling message');
     if (event.data.uresponse === 'skipwaiting') self.skipWaiting();
 });
